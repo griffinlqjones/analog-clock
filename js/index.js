408eddc7da16a551ded8a14c9cf51cc0eaa4391c
Griffin Jones <griffinlqjones@gmail.com>
2019-07-26 15:52:58 -0700

    Refactor

--- js/index.js
+++ js/index.js
@@ -34,6 +34,71 @@
 //
 // console.log('Completed');
 
+class ClockManager {
+  constructor(secondHand, minuteHand, hourHand) {
+    this.date = this.getTime();
+    this.rotations = null;
+    this.secondHand = secondHand;
+    this.minuteHand = minuteHand;
+    this.hourHand = hourHand;
+
+  }
+
+  getTime() {
+    let date = new Date()
+    return {
+      hours: date.getHours(),
+      minutes: date.getMinutes(),
+      seconds: date.getSeconds()
+    }
+  }
+
+  toRotation(date) {
+    /* minutes = x/60 : y/360*/
+    /* minutes = x/12 : y/360*/
+    return {
+      hours: (date.hours * 30) + 'deg',
+      minutes: (date.minutes * 6) + 'deg',
+      seconds: (date.seconds * 6) + 'deg'
+    }
+  }
+
+  zeroDegreeCorrection(lastRotation, newRotation, hand) {
+    if (newRotation < lastRotation) {
+      hand.classList.add('transform-disabled');
+
+      hand.style.transform = 'rotate(' + rotations.hours + ')';
+      hand.classList.remove('transform-disabled');
+    }
+  }
+
+  hourCorrection(hour) {
+    if(hour > 12) {
+      hour = hour - 12;
+    }
+    if(hour < 1) {
+      hour = 12;
+    }
+    return hour < 10 ? '0' + hour : hour;
+  }
+
+  minuteCorrection(minutes) {
+    return minutes < 10 ? '0' + minutes : minutes;
+  }
+  // Function to update the time
+  updateTime() {
+    time = getTime();
+    rotations = toRotation(time);
+
+    hourHand.style.transform = 'rotate(' + rotations.hours + ')';
+    minuteHand.style.transform = 'rotate(' + rotations.minutes + ')';
+    secondHand.style.transform = 'rotate(' + rotations.seconds + ')';
+    console.log('Tick');
+    digitalClockHour.innerText = hourCorrection(time.hours);
+    digitalClockMinutes.innerText = minuteCorrection(time.minutes);
+  }
+}
+
 
 let digitalClockHour = document.querySelector('#digital-clock .hour-value');
 let digitalClockMinutes = document.querySelector('#digital-clock .minute-value');
@@ -42,53 +107,64 @@ let hourHand = document.querySelector('#hour-hand');
 let minuteHand = document.querySelector('#minute-hand');
 let secondHand = document.querySelector('#second-hand');
 
+let clock = new ClockManager(secondHand, minuteHand, hourHand);
 
-function toRotation(date) {
-  /* minutes = x/60 : y/360*/
-  /* minutes = x/12 : y/360*/
-  return {
-    hours: (date.hours * 30) + 'deg',
-    minutes: (date.minutes * 6) + 'deg',
-    seconds: (date.seconds * 6) + 'deg'
-  }
-}
 
-function getTime() {
-  let date = new Date()
-  return {
-    hours: date.getHours(),
-    minutes: date.getMinutes(),
-    seconds: date.getSeconds()
-  }
-}
+// let time = getTime();
+// let rotations = toRotation(time);
+// updateTime();
+// setInterval(updateTime, 1000);
 
-function hourCorrection(hour) {
-  if(hour > 12) {
-    hour = hour - 12;
-  }
-  if(hour < 1) {
-    hour = 12;
-  }
-  return hour < 10 ? '0' + hour : hour;
-}
-
-function minuteCorrection(minutes) {
-  return minutes < 10 ? '0' + minutes : minutes;
-}
-// Function to update the time
-function updateTime() {
-  time = getTime();
-  rotations = toRotation(time);
-
-  hourHand.style.transform = 'rotate(' + rotations.hours + ')';
-  minuteHand.style.transform = 'rotate(' + rotations.minutes + ')';
-  secondHand.style.transform = 'rotate(' + rotations.seconds + ')';
-  console.log('Tick');
-  digitalClockHour.innerText = hourCorrection(time.hours);
-  digitalClockMinutes.innerText = minuteCorrection(time.minutes);
-}
-
-let time = getTime();
-let rotations = toRotation(time);
-updateTime();
-setInterval(updateTime, 1000);
+// function toRotation(date) {
+//   /* minutes = x/60 : y/360*/
+//   /* minutes = x/12 : y/360*/
+//   return {
+//     hours: (date.hours * 30) + 'deg',
+//     minutes: (date.minutes * 6) + 'deg',
+//     seconds: (date.seconds * 6) + 'deg'
+//   }
+// }
+//
+// function zeroDegreeCorrection(lastRotation, newRotation, hand) {
+//   if (newRotation < lastRotation) {
+//     hand.classList.add('transform-disabled');
+//
+//     hand.style.transform = 'rotate(' + rotations.hours + ')';
+//     hand.classList.remove('transform-disabled');
+//   }
+// }
+//
+// function getTime() {
+//   let date = new Date()
+//   return {
+//     hours: date.getHours(),
+//     minutes: date.getMinutes(),
+//     seconds: date.getSeconds()
+//   }
+// }
+//
+// function hourCorrection(hour) {
+//   if(hour > 12) {
+//     hour = hour - 12;
+//   }
+//   if(hour < 1) {
+//     hour = 12;
+//   }
+//   return hour < 10 ? '0' + hour : hour;
+// }
+//
+// function minuteCorrection(minutes) {
+//   return minutes < 10 ? '0' + minutes : minutes;
+// }
+// // Function to update the time
+// function updateTime() {
+//   time = getTime();
+//   rotations = toRotation(time);
+//
+//   hourHand.style.transform = 'rotate(' + rotations.hours + ')';
+//   minuteHand.style.transform = 'rotate(' + rotations.minutes + ')';
+//   secondHand.style.transform = 'rotate(' + rotations.seconds + ')';
+//   console.log('Tick');
+//   digitalClockHour.innerText = hourCorrection(time.hours);
+//   digitalClockMinutes.innerText = minuteCorrection(time.minutes);
+// }
